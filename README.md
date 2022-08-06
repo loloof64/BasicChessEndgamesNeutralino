41316030c408f72c14e22ed670cca4ba669c2152
loloof64 <laurent.bernabe@gmail.com>
2022-08-06 12:01:22 +0200

    We can start a new game

--- README.md
+++ README.md
@@ -16,3 +16,12 @@ Generate a chess endgame and play it against your computer.
 1. Create an empty file **auth_info.json** in a folder **.tmp** at the root of the project. Or clear it if it already exists. This file must be empty before trying to run.
 2. Open a terminal in **frontend** folder :  execute `yarn serve`.
 3. Open another terminal in the root folder of the project: execute `yarn serve:neu`.
+
+## Credits
+
+### Vectors
+
+Some vectors have been downloaded from [SvgRepo](https://www.svgrepo.com)
+
+* start.svg from [Start](https://www.svgrepo.com/svg/304738/start-line)
+* reverse.svg from [Reverse](https://www.svgrepo.com/svg/215783/up-arrow-ui)
--- frontend/src/components/GamePage.vue
+++ frontend/src/components/GamePage.vue
@@ -1,26 +1,35 @@
 <script setup>
+import {ref} from "vue";
 
+const board = ref();
+
+function doStartNewGame() {
+    board.value.newGame();
+}
+
+async function purposeStartGame() {
+    let button = await Neutralino.os
+            .showMessageBox('Start a new game ?',
+                            'Do you want to start a new game ?',
+                            'YES_NO', 'QUESTION');
+    if(button == 'YES') {
+        doStartNewGame();
+    }
+}
 </script>
 
 <template>
-    <div id="root">
-         <h1 class="text-3xl font-bold bg-sky-600">
-            Hello world!
-        </h1>
-        <loloof64-chessboard 
+    <div class="box-border w-full h-full flex flex-col justify-center items-center">
+         <div class="flex flex-row justify-around items-center shadow-lg bg-gray-300 p-2">
+            <img src="@/assets/vectors/start.svg" alt="start game"
+                class="w-10 h-10"
+                @click="purposeStartGame"
+            />
+        </div>
+        <loloof64-chessboard
+            ref="board"
             size="450"
+            class="pt-6"
         />
     </div>
 </template>
-
-<style scoped>
-#root {
-    box-sizing: border-box;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
-</style>
