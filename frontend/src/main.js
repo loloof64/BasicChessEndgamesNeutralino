a4217c7ecf518f1541449a1e247b90a2450c3f8c
loloof64 <laurent.bernabe@gmail.com>
2022-08-07 11:45:47 +0200

    Resized icon image to the good size

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -22,10 +22,6 @@ const i18n = createI18n({
   messages,
 });
 
-window.addEventListener("contextmenu", function(event) {
-  event.preventDefault();
-}, {passive: false});
-
 createApp(App).use(i18n).mount("#app");
 
 Neutralino.init()
